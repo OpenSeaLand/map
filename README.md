dbf8b2f0f3426430a9691f765de78292949e11e8
0xmaster0 <0xcii8369@gmail.com>
2022-06-10 00:12:41 +0800

    init

---
+++ README.md
@@ -0,0 +1,21 @@
+
+
+#To run the dapp Locally
+
+### npm run dev
+
+Try running some of the following tasks:
+
+```shell
+npx hardhat compile
+npx hardhat accounts
+npx hardhat clean
+npx hardhat test
+npx hardhat node
+node scripts/sample-script.js
+npx hardhat help
+
+npx hardhat run scripts/deployContract.js --network rinkeby
+npx hardhat run scripts/verifyContract.js --network rinkeby
+ 
+```
---
+++ dapp.config.js
@@ -0,0 +1,59 @@
+const FORTMATIC_KEY = process.env.NEXT_PUBLIC_FORTMATIC_KEY
+const RPC_URL = process.env.NEXT_PUBLIC_ALCHEMY_RPC_URL
+
+const config = {
+  title: 'OpenSea Land Map',
+  description: 'OpenSea Land Map Contract',
+  contractAddress: '0x40fb62F1C4B226f0a7FE72a8Fe1f079506eF303d',
+  maxMintAmount: 2,
+  presaleMaxMintAmount: 3,
+  price: 0.01
+}
+
+const onboardOptions = {
+  dappId: process.env.NEXT_PUBLIC_DAPP_ID,
+  networkId: 4, // Rinkeby
+  darkMode: true,
+  walletSelect: {
+    wallets: [
+      { walletName: 'metamask', preferred: true },
+      { walletName: 'coinbase', preferred: true },
+      {
+        walletName: 'walletLink',
+        preferred: true,
+        rpcUrl: RPC_URL,
+        appName: 'BoredApe Dapp'
+      },
+      {
+        walletName: 'fortmatic',
+        apiKey: FORTMATIC_KEY,
+        preferred: true
+      },
+      { walletName: 'trust', preferred: true, rpcUrl: RPC_URL },
+      { walletName: 'gnosis', preferred: true },
+      { walletName: 'authereum' },
+
+      {
+        walletName: 'ledger',
+        rpcUrl: RPC_URL
+      },
+      {
+        walletName: 'lattice',
+        rpcUrl: RPC_URL,
+        appName: 'BoredApe Dapp'
+      },
+      {
+        walletName: 'keepkey',
+        rpcUrl: RPC_URL
+      }
+    ]
+  },
+  walletCheck: [
+    { checkName: 'derivationPath' },
+    { checkName: 'accounts' },
+    { checkName: 'connect' },
+    { checkName: 'network' }
+  ]
+}
+
+export { config, onboardOptions }
